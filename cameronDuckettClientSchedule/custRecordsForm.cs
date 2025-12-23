1d9420ce714b0b1dbe2b4ab5164fe25615d960fa
Cameron Duckett <114952118+Ducky-007@users.noreply.github.com>
2025-12-23 14:59:20 -0800

    adds add customer validation fields to form

--- cameronDuckettClientSchedule/custRecordsForm.cs
+++ cameronDuckettClientSchedule/custRecordsForm.cs
@@ -5,8 +5,11 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using cameronDuckettClientSchedule.Database;
+using MySql.Data.MySqlClient;
 
 namespace cameronDuckettClientSchedule
 {
@@ -19,17 +22,32 @@ namespace cameronDuckettClientSchedule
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //variables for trimmed text box values
+            string name = nameTextBox.Text.Trim();
+            string address = addressTextBox.Text.Trim();
+            string city = cityTextBox.Text.Trim();
+            string country = countryTextBox.Text.Trim();
+            string zipCode = zipCodeTextBox.Text.Trim();
+            string phoneNum = phoneNumTextBox.Text.Trim();
+
             //check and ensure all text boxes are filled
-            if (string.IsNullOrWhiteSpace(nameTextBox.Text) ||
-                string.IsNullOrWhiteSpace(addressTextBox.Text) ||
-                string.IsNullOrWhiteSpace(cityTextBox.Text) ||
-                string.IsNullOrWhiteSpace(countryTextBox.Text) ||
-                string.IsNullOrWhiteSpace(zipCodeTextBox.Text) ||
-                string.IsNullOrWhiteSpace(phoneNumTextBox.Text))
+            if (string.IsNullOrWhiteSpace(name) ||
+                string.IsNullOrWhiteSpace(address) ||
+                string.IsNullOrWhiteSpace(city) ||
+                string.IsNullOrWhiteSpace(country) ||
+                string.IsNullOrWhiteSpace(zipCode) ||
+                string.IsNullOrWhiteSpace(phoneNum))
             {
                 MessageBox.Show("Please fill in all fields.");
                 return;
             }
+            if (!Regex.IsMatch(phoneNum, @"^[0-9-]+$"))
+            {
+                MessageBox.Show("Phone number must contain only digits and dashes.");
+            }
+            // open connection
+            DBConnection.OpenConnection();
+
 
         }
 
