392ae77b68f8b8462c3de11ce4a38ed256acf197
Cameron Duckett <114952118+Ducky-007@users.noreply.github.com>
2026-01-07 18:44:35 -0800

    adds fields to update customer form

--- cameronDuckettClientSchedule/updateCustForm.Designer.cs
+++ cameronDuckettClientSchedule/updateCustForm.Designer.cs
@@ -36,6 +36,14 @@
             this.label6 = new System.Windows.Forms.Label();
             this.currName = new System.Windows.Forms.Label();
             this.custCurrInfo = new System.Windows.Forms.Label();
+            this.custNameTextBox = new System.Windows.Forms.TextBox();
+            this.custPhoneTextBox = new System.Windows.Forms.TextBox();
+            this.custZipTextBox = new System.Windows.Forms.TextBox();
+            this.custCityTextBox = new System.Windows.Forms.TextBox();
+            this.custAddressTextBox = new System.Windows.Forms.TextBox();
+            this.UpdateCustBtn = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.countryTextBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -52,7 +60,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(52, 116);
+            this.label2.Location = new System.Drawing.Point(47, 118);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(85, 13);
             this.label2.TabIndex = 1;
@@ -62,7 +70,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(69, 146);
+            this.label3.Location = new System.Drawing.Point(67, 165);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(48, 13);
             this.label3.TabIndex = 2;
@@ -71,7 +79,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(81, 178);
+            this.label4.Location = new System.Drawing.Point(71, 215);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(27, 13);
             this.label4.TabIndex = 3;
@@ -80,7 +88,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(52, 209);
+            this.label5.Location = new System.Drawing.Point(52, 299);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(53, 13);
             this.label5.TabIndex = 4;
@@ -89,7 +97,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(52, 239);
+            this.label6.Location = new System.Drawing.Point(34, 349);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(81, 13);
             this.label6.TabIndex = 5;
@@ -106,18 +114,87 @@
             // custCurrInfo
             // 
             this.custCurrInfo.AutoSize = true;
-            this.custCurrInfo.Location = new System.Drawing.Point(200, 79);
+            this.custCurrInfo.Location = new System.Drawing.Point(274, 83);
             this.custCurrInfo.Name = "custCurrInfo";
             this.custCurrInfo.Size = new System.Drawing.Size(85, 13);
             this.custCurrInfo.TabIndex = 11;
             this.custCurrInfo.Text = "Current Info Title";
             this.custCurrInfo.Click += new System.EventHandler(this.custCurrInfo_Click);
             // 
+            // custNameTextBox
+            // 
+            this.custNameTextBox.Location = new System.Drawing.Point(138, 116);
+            this.custNameTextBox.Name = "custNameTextBox";
+            this.custNameTextBox.Size = new System.Drawing.Size(137, 20);
+            this.custNameTextBox.TabIndex = 12;
+            // 
+            // custPhoneTextBox
+            // 
+            this.custPhoneTextBox.Location = new System.Drawing.Point(137, 346);
+            this.custPhoneTextBox.Name = "custPhoneTextBox";
+            this.custPhoneTextBox.Size = new System.Drawing.Size(137, 20);
+            this.custPhoneTextBox.TabIndex = 13;
+            // 
+            // custZipTextBox
+            // 
+            this.custZipTextBox.Location = new System.Drawing.Point(137, 296);
+            this.custZipTextBox.Name = "custZipTextBox";
+            this.custZipTextBox.Size = new System.Drawing.Size(137, 20);
+            this.custZipTextBox.TabIndex = 14;
+            // 
+            // custCityTextBox
+            // 
+            this.custCityTextBox.Location = new System.Drawing.Point(137, 215);
+            this.custCityTextBox.Name = "custCityTextBox";
+            this.custCityTextBox.Size = new System.Drawing.Size(137, 20);
+            this.custCityTextBox.TabIndex = 15;
+            // 
+            // custAddressTextBox
+            // 
+            this.custAddressTextBox.Location = new System.Drawing.Point(137, 165);
+            this.custAddressTextBox.Name = "custAddressTextBox";
+            this.custAddressTextBox.Size = new System.Drawing.Size(137, 20);
+            this.custAddressTextBox.TabIndex = 16;
+            // 
+            // UpdateCustBtn
+            // 
+            this.UpdateCustBtn.Location = new System.Drawing.Point(166, 392);
+            this.UpdateCustBtn.Name = "UpdateCustBtn";
+            this.UpdateCustBtn.Size = new System.Drawing.Size(75, 23);
+            this.UpdateCustBtn.TabIndex = 17;
+            this.UpdateCustBtn.Text = "UPDATE";
+            this.UpdateCustBtn.UseVisualStyleBackColor = true;
+            this.UpdateCustBtn.Click += new System.EventHandler(this.UpdateCustBtn_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(59, 259);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Country:";
+            // 
+            // countryTextBox
+            // 
+            this.countryTextBox.Location = new System.Drawing.Point(137, 256);
+            this.countryTextBox.Name = "countryTextBox";
+            this.countryTextBox.Size = new System.Drawing.Size(137, 20);
+            this.countryTextBox.TabIndex = 19;
+            // 
             // updateCustForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.countryTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.UpdateCustBtn);
+            this.Controls.Add(this.custAddressTextBox);
+            this.Controls.Add(this.custCityTextBox);
+            this.Controls.Add(this.custZipTextBox);
+            this.Controls.Add(this.custPhoneTextBox);
+            this.Controls.Add(this.custNameTextBox);
             this.Controls.Add(this.custCurrInfo);
             this.Controls.Add(this.currName);
             this.Controls.Add(this.label6);
@@ -144,5 +221,13 @@
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label currName;
         private System.Windows.Forms.Label custCurrInfo;
+        private System.Windows.Forms.TextBox custNameTextBox;
+        private System.Windows.Forms.TextBox custPhoneTextBox;
+        private System.Windows.Forms.TextBox custZipTextBox;
+        private System.Windows.Forms.TextBox custCityTextBox;
+        private System.Windows.Forms.TextBox custAddressTextBox;
+        private System.Windows.Forms.Button UpdateCustBtn;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox countryTextBox;
     }
 }
--- cameronDuckettClientSchedule/updateCustForm.cs
+++ cameronDuckettClientSchedule/updateCustForm.cs
@@ -7,6 +7,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -26,7 +27,7 @@ namespace cameronDuckettClientSchedule
         private void updateCustForm_Load(object sender, EventArgs e)
         {
             label1.Text = $"UPDATE {_custName.ToUpper()}'S INFORMATION";
-            custCurrInfo.Text = $"Enter Information into all fields you want to update for {_custName}";
+            custCurrInfo.Text = $"Enter Information into all fields to update for {_custName}";
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -42,5 +43,37 @@ namespace cameronDuckettClientSchedule
         private void label1_Click(object sender, EventArgs e)
         {
         }
+
+        private void UpdateCustBtn_Click(object sender, EventArgs e)
+        {
+            //if any textboxes are empty, do nothing
+            string name = custNameTextBox.Text.Trim();
+            string address = custAddressTextBox.Text.Trim();
+            string city = custCityTextBox.Text.Trim();
+            string zip = custZipTextBox.Text.Trim();
+            string country = countryTextBox.Text.Trim();
+            string phone = custPhoneTextBox.Text.Trim();
+            if (string.IsNullOrWhiteSpace(name) ||
+                string.IsNullOrWhiteSpace(address) ||
+                string.IsNullOrWhiteSpace(city) ||
+                string.IsNullOrWhiteSpace(zip) ||
+                string.IsNullOrWhiteSpace(country) ||
+                string.IsNullOrWhiteSpace(phone))
+            {
+                MessageBox.Show($"Enter values into all fields!");
+            }
+            if (!Regex.IsMatch(phone, @"^[0-9-]+$"))
+            {
+                MessageBox.Show("Phone number must contain only digits and dashes.");
+            }
+
+            //run query to insert values into each table accordingly
+            //start with countryId
+            DBConnection.OpenConnection();
+            string getCountryIdQuery = $"SELECT countryId from country where country = {country}";
+            MySqlCommand countryIdCmd = new MySqlCommand(getCountryIdQuery, DBConnection.conn);
+            //create reader to finish query
+        }
     }
 }
+
